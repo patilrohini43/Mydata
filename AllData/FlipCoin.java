1caf167573e889a105407a24b1a4d16f7133369e
patilrohini43 <45816823+patilrohini43@users.noreply.github.com>
2018-12-21 16:38:17 +0530

    Program

--- AllData/FlipCoin.java
+++ AllData/FlipCoin.java
@@ -8,35 +8,17 @@
 
 package all.program.com;
 
+import all.program.utility.Utility;
+
 public class FlipCoin {
 	public static void main(String args[])
+	
 	{
-		int loopCount=1;
-		double result;
-		int heads=0;
-		
-		while(loopCount<=10)
-		{
-			/**
-			 * math.random() produces results >= 0.0 and < 1.0
-			 */
-			result=Math.random();
-			if(result <= .5)
-				System.out.println("this is heads");
-			if(result <= .5)
-				heads=heads++;
-			else
-				System.out.println("this is tails");
-				loopCount=loopCount+1;
-				
-		}
-		/**
-		 * calculate percentage
-		 */
-		double perHeads=heads/10*100;
-		double perTails=(10-heads)/10*100;
-	System.out.println("Percentage of Heads"+ perHeads);
-	System.out.println("Percentage of Heads"+ perTails);
+		Utility u=new Utility();
+		//accept the value from user
+		System.out.println("enter  no of times  to flip the coin");
+		int times = u.inputInteger();
+		//call the perFlip method
+        u.perFlip(times);
 	}
-
 }
